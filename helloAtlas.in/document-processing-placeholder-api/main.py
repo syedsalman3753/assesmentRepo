de5ce2dd335076442e67e0b9ac586556b415d83c
syed salman <syedsalman3753@gmail.com>
2025-12-05 19:42:55 +0530

    Developed simple API
    Signed-off-by: syed salman <syedsalman3753@gmail.com>

---
+++ helloAtlas.in/document-processing-placeholder-api/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+@app.post("/process")
+async def process_file(file: UploadFile = File(...)):
+    return JSONResponse({"status": "ok", "filename": file.filename})
